60420f218062b93987c799d0c324a6f7bfe9600f
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2026-04-02 22:13:46 +0500

    Payshanba: Middleware qo'shildi va testlar o'tkazildi

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-// JSON body qabul qilish uchun
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Fake database
+let users = [
+  { id: 1, name: 'Ali' },
+  { id: 2, name: 'Vali' },
+];
+
+// Home routes
 app.get('/', (req, res) => {
   res.send('Salom, Express! Server ishlamoqda 🚀');
 });
@@ -17,28 +26,64 @@ app.get('/about', (req, res) => {
   res.send('Bu haqida sahifa. Backend o‘rganayapmiz!');
 });
 
-// POST route
+// 🔥 GET all users (Postman uchun)
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// 🔥 GET single user
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = users.find((u) => u.id == id);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User topilmadi' });
+  }
+
+  res.json(user);
+});
+
+// POST
 app.post('/users', (req, res) => {
   const { name } = req.body;
 
+  const newUser = {
+    id: users.length + 1,
+    name,
+  };
+
+  users.push(newUser);
+
   res.status(201).json({
-    message: `Yangi user qo'shildi: ${name}`,
+    message: "Yangi user qo'shildi",
+    user: newUser,
   });
 });
 
-// PUT route
+// PUT
 app.put('/users/:id', (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
+  const user = users.find((u) => u.id == id);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User topilmadi' });
+  }
+
+  user.name = name;
+
   res.json({
-    message: `User ${id} yangilandi: ${name}`,
+    message: 'User yangilandi',
+    user,
   });
 });
 
-// DELETE route
+// DELETE
 app.delete('/users/:id', (req, res) => {
   const { id } = req.params;
+
+  users = users.filter((u) => u.id != id);
 
   res.json({
     message: `User ${id} o‘chirildi`,
